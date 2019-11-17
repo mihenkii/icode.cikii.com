207ff9858cb8d808f225449abdd3f78255138938
yanyingjie <yanyingjie@baidu.com>
2019-11-17 21:23:24 +0800

    init mongodb

--- cikii/xiaofu/jiuserver/main.go
+++ cikii/xiaofu/jiuserver/main.go
@@ -1,10 +1,33 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+
 	"github.com/gin-gonic/gin"
+	// "go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type Trainer struct {
+	Name	string
+	Age	int
+	City	string
+}
+
 func main() {
+	clientOptions := options.Client().ApplyURI("mongodb+srv://cikii:1Fys0F1vzWjKkKGE@cluster0-oqjxi.azure.mongodb.net/test?retryWrites=true&w=majority")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB")
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
